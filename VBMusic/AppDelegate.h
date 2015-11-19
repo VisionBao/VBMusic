e539c9d7c60408d819e6847dac98489bd7e95caf
VisionBao <Vision8888888@gmail.com>
2015-11-19 23:30:40 +0800

    init project

---
+++ VBMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VBMusic
+//
+//  Created by Vision on 15/11/19.
+//  Copyright © 2015年 VisionBao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VBMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VBMusic
+//
+//  Created by Vision on 15/11/19.
+//  Copyright © 2015年 VisionBao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
